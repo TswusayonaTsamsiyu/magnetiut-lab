666022d99390c0dcec65f5919f5eaa768a8c4e17
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2022-05-08 23:42:04 +0300

    Added code for accessing filesystem

---
+++ src/mgnt/fs.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from socket import gethostname
+
+GABI_LAPTOP = "LAPTOP-FFTKFOU5"
+GABI_LAB_DIR = Path("H:/") / "My Drive" / "Labs"
+ILAY_LAB_DIR = Path("G:/") / ".shortcut-targets-by-id" / "1qgY2gJharU3uwILQitDrQ7FqFqe7hwU0"
+LAB_DIR = GABI_LAB_DIR if gethostname() == GABI_LAPTOP else GABI_LAB_DIR
+MAGNETISM_DIR = LAB_DIR / "Physics Lab B" / "4. Magnetism"
+PERMEABILITY_DIR = MAGNETISM_DIR / "1. Permeability"
+
+
+def is_csv(path: Path) -> bool:
+    return path.suffix == ".csv"
+
+
+def get_material_path(material: str) -> Path:
+    return PERMEABILITY_DIR / material.upper()
+
+
+def get_measurement_path(material: str, resistance: int) -> Path:
+    return get_material_path(material) / f"{resistance}k.csv"
+
+
+def iter_material(material: str):
+    return filter(is_csv, get_material_path(material).iterdir())
