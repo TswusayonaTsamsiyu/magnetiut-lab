40ab6daee76f119009c787d72ff423ef95d150fb
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2022-05-08 21:24:44 +0300

    Created package file structure

---
+++ src/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mgnt",
+    version="1.0",
+    packages=find_packages(),
+    description="The most awesomest magnetiest package"
+)
