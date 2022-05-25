39b332ae0964d24a6901ba00ba4f1d6bd8363196
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2022-05-25 14:44:50 +0300

    Added better otsu method

--- src/mgnt/domains/analysis.py
+++ src/mgnt/domains/analysis.py
@@ -1,11 +1,13 @@
 import cv2 as cv
+from skimage.io import imread
 
 from .currents import get_current
 from .fs import iter_images, parse_voltage
 
 
 def read_image(path):
-    return cv.imread(str(path))
+    # return cv.imread(str(path))
+    return imread(str(path), as_gray=True)
 
 
 def get_black_ratio(img):
@@ -16,6 +18,6 @@ def generate_data(mask):
     for path in iter_images():
         try:
             image = read_image(path)
-            yield get_current(parse_voltage(image)), get_black_ratio(mask(image))
+            yield get_current(parse_voltage(path)), get_black_ratio(mask(image))
         except KeyError:
             pass
--- src/mgnt/domains/masking.py
+++ src/mgnt/domains/masking.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+from skimage.filters import threshold_otsu
 
 _LOWER = (0, 0, 20)
 _UPPER = (100, 100, 200)
@@ -7,6 +8,10 @@ _RED_BOUNDS = (_LOWER, _UPPER)
 
 def otsu(image):
     return cv.threshold(cv.cvtColor(image, cv.COLOR_BGR2GRAY), 128, 192, cv.THRESH_OTSU)[1]
+
+
+def better_otsu(image):
+    return image > threshold_otsu(image)
 
 
 def red(image):
