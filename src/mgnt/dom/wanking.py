da8afdb9883c2899d67d31df62a657c1d11bfa73
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2022-06-03 18:07:17 +0300

    Better graph

--- src/mgnt/dom/wanking.py
+++ src/mgnt/dom/wanking.py
@@ -1,9 +1,10 @@
+import numpy as np
 from skimage.io import imshow
 from matplotlib import pyplot as plt
 from skimage.filters import threshold_otsu
 
 from mgnt.common.plotting import plot
-from mgnt.common.utils import read_columns
+from mgnt.common.utils import read_columns, unzip
 
 from .currents import get_current
 from .analysis import read_image, get_black_ratio
@@ -24,6 +25,23 @@ def save(voltage, mask):
         f.write(f"{get_current(voltage)}, {get_black_ratio(mask)}\n")
 
 
+def sort_by_current(i, m):
+    return unzip(sorted(zip(i, m)))
+
+
+def neg(items):
+    return -np.array(items)
+
+
 def plot_loop():
     with plot() as axes:
-        axes.scatter(*list(read_columns(DOMAINS_DIR / "data.csv", [0, 1])))
+        i, m = read_columns(DOMAINS_DIR / "data.csv", [0, 1])
+        ia, ma = sort_by_current(i[:30], m[:30])
+        ib, mb = sort_by_current(i[30:], m[30:])
+        ia, ib = -np.array(ia), -np.array(ib)
+        ma, mb = np.array(ma) - 0.5, np.array(mb) - 0.5
+        axes.plot(ia, ma, label="a")
+        axes.plot(ib, mb, label="b")
+        axes.scatter(ia, ma, label="a")
+        axes.scatter(ib, mb, label="b")
+        axes.legend()
